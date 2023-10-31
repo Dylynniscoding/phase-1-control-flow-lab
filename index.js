14493da10d4423df8ee1db762d5e84c92992dcb7
Dylynn Barrows <DylynnBarrows27@gmail.com>
2023-10-30 20:18:40 -0700

    fix

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function scuberGreetingForFeet(feet) {
   let response;
   if (feet <= 400) {
     response = 'This one is on me!';
-  else if (feet > 400 && feet <= 2000) {
-    response = 'That will be twenty bucks.';
+    else if (feet > 400 && feet <= 2000) {
+      response = 'That will be twenty bucks.';
+    }
   } else if (feet > 2000 && feet <= 2500) {
     response = 'I will gladly take your thirty bucks.';
   } else if (feet > 2500) {

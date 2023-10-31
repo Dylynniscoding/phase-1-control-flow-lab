5a9cec10d1f41a77231500865d97a79872ee2921
Dylynn Barrows <DylynnBarrows27@gmail.com>
2023-10-30 20:29:29 -0700

    this time for sure

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 function scuberGreetingForFeet(){
   // Write your code here!
-  if (ride <= 400) {
-    return 'This one is on me!' 
-  } 
-    else if (ride > 400 && ride <= 2000) {
-      return 'That will be twenty bucks.'  
+  function scuberGreetingForFeet (distance) {
+    if (distance <= 400) {
+      return 'This one is on me!';
     }
-    else if (ride > 2000 && ride <= 2500) {
-      return 'I will gladly take your thirty bucks.'
+    else if (distance > 400 && distance <= 2000) {
+      return 'That will be twenty bucks.';
+    }
+    else if (distance > 2000 && distance <= 2500) {
+      return 'I will gladly take your thirty bucks.';
     }
     else {
-      return 'No can do.'
+      return 'No can do.';
     }
-}
-
-
-
-function ternaryCheckCity(city){
-  // Write your code here!
-  return city === 'NYC' ? "Ok, sounds good." : "No go.";
-}
-
-
-function switchOnCharmFromTip(tip){
-  // Write your code here!
-  switch (tip) {
-    case 'generous':
-      return "Thank you so much."
-    case 'not as generous':
-      return "Thank you."
-    default:
-      return "Bye."
   }
-}
+  // tenrenary operator
+  function ternaryCheckCity (city) {
+   return city === 'NYC'? 'Ok, sounds good.': 'No go.';
+  }
+  
+  // switch statements
+  function switchOnCharmFromTip (tip) {
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
+  }

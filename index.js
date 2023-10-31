6f5a2874f151c4ee5f72c66953feb0570b04b983
Dylynn Barrows <DylynnBarrows27@gmail.com>
2023-10-30 20:12:03 -0700

    tests

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 function scuberGreetingForFeet(){
   // Write your code here!
-}
+  if scuberGreetingForFeet === 199 {
+    return "This one is on me!";
+  }
+  else if scuberGreetingForFeet === 1500 {
+    return "That will be twenty bucks.";
+  }
+  else if scuberGreetingForFeet === 2001 {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if scuberGreetingForFeet === 2501 {
+    return "No can do.";
+  }
+
+
 
 function ternaryCheckCity(){
   // Write your code here!
+  if ternaryCheckCity === "NYC" {
+    return "Ok, sounds good.";
+  }
 }
+
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  if switchOnCharmFromTip === "generous" {
+    return "Thank you so much.";
+  }
 }
